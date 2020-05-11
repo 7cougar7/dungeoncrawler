9753f6e914dc417b6598ed7e588dcf704115ea1f
Marc Abdallah <marcabdallah32@gmail.com>
2020-05-11 17:08:41 -0500

    Added Enemy header file. WiP but had to restart my laptop and didn't want to lose progress

---
+++ Enemy.h
@@ -0,0 +1,17 @@
+//
+// Created by Marc Abdallah on 5/11/2020.
+//
+
+#pragma once
+
+class Enemy{
+    protected:
+        //FixMe
+        //SpriteDataType sprite;
+        string name;
+        string enemyTip;
+        bool aware;
+    public:
+        Enemy();
+        Enemy(string name, string enemyTip)
+};
