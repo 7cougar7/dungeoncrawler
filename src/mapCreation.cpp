d8f6336fa304a8be0e6fdd8bf0478b05b3cc92d9
Tilo Garcia <tilogarcia1@gmail.com>
2020-05-12 01:43:40 -0500

    Added line creation function

--- src/mapCreation.cpp
+++ src/mapCreation.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <ctime>
 #include <cstdlib>
+#include <cmath>
 #include "Tile.h"
 
 const int possibleDirections[4][2] = {
@@ -24,7 +25,7 @@ std::vector<std::vector<Tile>> levelLayout(maxX ,std::vector<Tile> (maxY)); //Ou
 //xCoord/yCoord = bottom left corner coordinates
 //Success = 0; Fail = 1
 int createSquare(int size, int xCoord, int yCoord) {
-    if(xCoord+size > maxX || yCoord+size > maxY){
+    if(xCoord+size > maxX || yCoord+size > maxY || xCoord < 0 || yCoord < 0){
         return 1;
     }
     for(int x = yCoord-1; x < yCoord+size-1; x++){
@@ -35,13 +36,60 @@ int createSquare(int size, int xCoord, int yCoord) {
     return 0;
 }
 
+//T = % through line
+double lerp(int start, int end, double T){
+    return start+(T*(end-start));
+}
+
+int createLine(int xStart, int yStart, int xEnd, int yEnd){
+    if(xStart > maxX || yStart > maxY || xEnd > maxX || yEnd > maxY
+        || xStart < 0 || yStart < 0 || xEnd < 0 || yEnd < 0){
+            return 1;
+    }
+    if(xStart == xEnd){ //Vertical Line
+        for(int y = yStart; y<yEnd-yStart+2; y++){
+            levelLayout[y][xStart] = *path;
+        }
+    } else if(yStart == yEnd){ //Horizontal Line
+        for(int x = xStart; x < xEnd-xStart+2; x++){
+            levelLayout[yStart][x] = *path;
+        }
+    } else { //Diagonal Line
+        int xDis = xEnd-xStart;
+        int yDis = yEnd-yStart;
+        double xAbsDis = abs(xEnd-xStart);
+        double yAbsDis = abs(yEnd-yStart);
+        int xSign = 1;
+        if(xDis < 0){ xSign = -1;}
+        int ySign = 1;
+        if(yDis < 0){ ySign = -1;}
+        int x = xStart;
+        int y = yStart;
+        levelLayout[y][x] = *path;
+        for (int ix = 0, iy = 0; ix < xAbsDis || iy < yAbsDis;) {
+            if ((0.5+ix) / xAbsDis < (0.5+iy) / yAbsDis) {
+                // next step is horizontal
+                x += xSign;
+                ix++;
+            } else {
+                // next step is vertical
+                y += ySign;
+                iy++;
+            }
+            levelLayout[y][x] = *path;
+        }
+    }
+    return 0;
+}
+
 int main(){
     srand(time(NULL));
-//    for(int i=0;i<15;i++) {
-//        levelLayout[rand() % maxX][rand() % maxY] = *path;
+    //levelLayout[0][0] = *path;
+    //createSquare(6,maxX/2-2,maxY/2-2);
+    createLine(1,1,10,16);
+//    if(createLine(1,1,5,1)){
+//        std::cout << "error";
 //    }
-    levelLayout[0][0] = *path;
-    createSquare(6,maxX/2-2,maxY/2-2);
     for (int x = maxY-1; x >= 0; x--) {
         for (int y = 0; y < maxX; y++){
             if(levelLayout[x][y].getSymbol() != '@'){
