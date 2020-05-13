2a5265726a5b899095f4991406e95aa868a0ec0a
Bach <zacharys0117@gmail.com>
2020-05-12 23:11:16 -0500

    Added SDL support

---
+++ mapCreation.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <cmath>
+#include "Tile.h"
+
+const int maxX = 50;
+const int maxY = 50;
+
+const int possibleDirections[4][2] = {
+        {1,0},
+        {-1,0},
+        {0,1},
+        {0,-1}
+};
+
+//extern std::vector<std::vector<Tile>> levelLayout(maxX, std::vector<Tile>(maxY));  //Outside Vector(x), Inside Vector(y)
+
+int createSquare(int size, int xCoord, int yCoord);
+int createLine(int xStart, int yStart, int xEnd, int yEnd, int thickness);
+void createRooms(int number, int minSize, int maxSize);
+void connectRooms();
+int initLevel();
